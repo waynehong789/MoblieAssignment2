6eb2224c949c312e8949eac81577ac83fc668b92
Daniel T <pdantech@gmail.com>
2017-05-25 14:50:09 +1200

    Added GoogleAPI places functions

--- app/src/main/java/com/example/FortuneStar/FoodGrab/SearchActivity.java
+++ app/src/main/java/com/example/FortuneStar/FoodGrab/SearchActivity.java
@@ -1,13 +1,59 @@
 package com.example.FortuneStar.FoodGrab;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class SearchActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+public class SearchActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private String TAG = "SearchActivity";
+
+    private int PLACE_PICKER_REQUEST = 1;
+
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build();
+    }
+
+    public void findPlaces(){
+        try {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
+        } catch (Exception ex) {
+            Log.e(TAG, "Exception finding places: " + ex.toString());
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String toastMsg = String.format("Place: %s", place.getName());
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "Error occurred connecting to google");
     }
 }
